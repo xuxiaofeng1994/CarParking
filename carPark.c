962af1b734f6611a5c7d21d6067d7a1daa1f4b95
xuxiaofeng1994 <xuxiaofeng1994@gmail.com>
2013-11-08 22:00:11 -0500

    修改车库和便道的结构（这应该是上一步完成的，没提交上）

--- carPark.c
+++ carPark.c
@@ -1,5 +1,5 @@
-#define CARPARKMAX 10
-#define TEMPCARPARKMAX 10
+#define CAR_MAX 10
+#define CAR_TEMP_MAX 10
 
 /*
 	定义车辆信息的结构体
@@ -13,15 +13,26 @@ typedef struct node
 	int priceHour;
 	int priceTotal;
 	struct node * next;;
-}
+}carInfo;
 
 /*
 	定义车库的数组
 */
-static int carPark[CARPARKMAX];
+struct 
+{
+	static STACK_TYPE park[CAR_MAX];
+	static int topElement = -1;
+	static int const lengthMax = CAR_MAX;
+
+}carPark;
 
 /*
 	定义用于便道的数组
 */
-static int tempCarPark[TEMPCARPARKMAX];
+struct 
+{
+	static STACK_TYPE park[CAR_TEMP_MAX];
+	static int topElementi = -1;
+	static const lengthMax = CAR_TEMP_MAX;
+}carParkTemp;
 
